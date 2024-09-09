8faf9716cdc5f87401a4ead2ee843d0d8d4b8732
Bernardo Quina <bquina@primeit.pt>
2024-09-09 20:48:03 +0100

    Fix github action - test

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -4,7 +4,11 @@ export default function Home() {
   const { data } = trpc.test.useQuery();
 
   if (!data) {
-    return <div>Loading...</div>;
+    return (
+      <div>
+        <p> Loading...</p>
+      </div>
+    );
   }
 
   return <div>{data.message}</div>;
