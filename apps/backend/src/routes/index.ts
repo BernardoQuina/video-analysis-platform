78dd03adb9bdfe7cc0e2dc31a57bb66180fe31a3
Bernardo Quina <bquina@primeit.pt>
2024-09-12 17:32:51 +0100

    Fix github action - backend- test 6

--- apps/backend/src/routes/index.ts
+++ apps/backend/src/routes/index.ts
@@ -19,7 +19,7 @@ export const appRouter = router({
       workerId: cluster.worker?.id,
       instanceId,
       instanceIp,
-      message: 'Deployment test message #2',
+      message: 'Deployment test message #3',
     };
   }),
 });
