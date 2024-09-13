17242cf5f9948347e2651ab78f4a717b5336c9b3
Bernardo Quina <bquina@primeit.pt>
2024-09-13 09:17:01 +0100

    Fix github action - backend- test 14

--- apps/backend/src/routes/index.ts
+++ apps/backend/src/routes/index.ts
@@ -2,8 +2,33 @@ import cluster from 'node:cluster';
 
 import { publicProcedure, router } from './trpc';
 
+// Function to simulate CPU load
+function stressTest(durationInSeconds: number) {
+  const end = Date.now() + durationInSeconds * 1000;
+
+  // Intensive CPU calculations (tight loop)
+  while (Date.now() < end) {
+    let result = 0;
+    for (let i = 0; i < 1e6; i++) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      result += Math.sqrt(i);
+    }
+  }
+  console.log(`CPU stress test finished after ${durationInSeconds} seconds`);
+}
+
 export const appRouter = router({
   test: publicProcedure.query(async () => {
+    // Start stress test for 30 seconds
+    stressTest(120);
+
+    return {
+      workerId: cluster.worker?.id,
+      instanceId: '',
+      instanceIp: '',
+      message: 'Deployment test message #14',
+    };
+
     // Fetch EC2 instance id and private ip
     const instanceIdResponse = await fetch(
       'http://169.254.169.254/latest/meta-data/instance-id',
@@ -19,7 +44,7 @@ export const appRouter = router({
       workerId: cluster.worker?.id,
       instanceId,
       instanceIp,
-      message: 'Deployment test message #13',
+      message: 'Deployment test message #14',
     };
   }),
 });
