ec8e0c9ee1c2b78ea170797aacb5d71fae9f5d56
Bernardo Quina <bquina@primeit.pt>
2024-09-17 20:26:41 +0100

    Add title and description metadata to main page

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -1,4 +1,12 @@
+import { Metadata } from 'next';
+
 import { trpc } from '../utils/trpc';
+
+export const metadata: Metadata = {
+  title: 'Video Analysis Demo',
+  description:
+    'This is demo project to showcase how you can deploy complex applications on AWS infrastructure, making use of multiple services like S3, CloudFront, ECS, SQS, DynamoDB and more.',
+};
 
 export default function Home() {
   const { data } = trpc.test.useQuery();
