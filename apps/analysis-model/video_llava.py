2e5de20336c4dfe32961aac517755c127c571f24
Bernardo Quina <bquina@primeit.pt>
2024-10-22 13:09:16 +0100

    Try creating docker image with model again

--- apps/analysis-model/video_llava.py
+++ apps/analysis-model/video_llava.py
@@ -41,7 +41,6 @@ def process_video(video_url, prompt):
 
     model = VideoLlavaForConditionalGeneration.from_pretrained(
         model_name,
-        cache_dir="/home/ec2-user/.cache/huggingface/hub",
         device_map="auto",
         attn_implementation=None,
     )
