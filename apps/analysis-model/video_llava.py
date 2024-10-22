380f7c84991f8aa752db7ea5d187ae720c0882b4
Bernardo Quina <bquina@primeit.pt>
2024-10-22 12:42:10 +0100

    Debug python script #15

--- apps/analysis-model/video_llava.py
+++ apps/analysis-model/video_llava.py
@@ -3,7 +3,12 @@ import numpy as np
 import requests
 import tempfile
 import os
-from transformers import VideoLlavaProcessor, VideoLlavaForConditionalGeneration
+import torch
+from transformers import (
+    VideoLlavaProcessor,
+    VideoLlavaForConditionalGeneration,
+    BitsAndBytesConfig,
+)
 
 
 def download_video(url):
@@ -39,8 +44,15 @@ def process_video(video_url, prompt):
     processor.patch_size = 14
     processor.vision_feature_select_strategy = "default"
 
+    # specify how to quantize the model
+    quantization_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.float16,
+    )
+
     model = VideoLlavaForConditionalGeneration.from_pretrained(
-        model_name, device_map="auto", attn_implementation=None
+        model_name, device_map="auto", quantization_config=quantization_config
     )
 
     # Download video
