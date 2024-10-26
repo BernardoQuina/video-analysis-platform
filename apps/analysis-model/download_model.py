ac2f9a5ce0bc27592477ec18ef52694c1809b70e
Bernardo Quina <bquina@primeit.pt>
2024-10-26 13:07:19 +0100

    Analysis model - try to fix slow shard loading and fix versions

--- apps/analysis-model/download_model.py
+++ apps/analysis-model/download_model.py
@@ -1,14 +1,24 @@
-from transformers import VideoLlavaProcessor, VideoLlavaForConditionalGeneration
+import torch
+from transformers import (
+    VideoLlavaProcessor,
+    VideoLlavaForConditionalGeneration,
+    BitsAndBytesConfig,
+)
 import config
 
 # Download and cache the processor
 processor = VideoLlavaProcessor.from_pretrained(config.MODEL_NAME)
+
+# Configure quantization
+quantization_config = BitsAndBytesConfig(
+    load_in_8bit=True, bnb_4bit_compute_dtype=torch.float16
+)
 
 # Download and cache the model
 model = VideoLlavaForConditionalGeneration.from_pretrained(
     config.MODEL_NAME,
     device_map="auto",
     attn_implementation="sdpa",
-    #    quantization_config=quantization_config,
-    #    torch_dtype=torch.float16
+    quantization_config=quantization_config,
+    torch_dtype=torch.float16,
 )
