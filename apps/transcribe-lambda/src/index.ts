c446c72738e1004a34226961feb52bbc6b26f9cf
Bernardo Quina <bquina@primeit.pt>
2024-10-30 10:26:08 +0000

    Transcribe Lambda - test first job

--- apps/transcribe-lambda/src/index.ts
+++ apps/transcribe-lambda/src/index.ts
@@ -1,10 +1,10 @@
 /* eslint-disable no-console */
 // import { S3Client } from '@aws-sdk/client-s3';
-// import {
-//   TranscribeClient,
-//   StartTranscriptionJobCommand,
-//   // StartTranscriptionJobCommandInput,
-// } from '@aws-sdk/client-transcribe';
+import {
+  TranscribeClient,
+  StartTranscriptionJobCommand,
+  StartTranscriptionJobCommandInput,
+} from '@aws-sdk/client-transcribe';
 import {
   APIGatewayProxyResult,
   EventBridgeEvent,
@@ -12,48 +12,40 @@ import {
   // eslint-disable-next-line node/no-missing-import
 } from 'aws-lambda';
 
-// const s3Client = new S3Client({});
-// const transcribeClient = new TranscribeClient({});
-
 export const handler = async (
   event: EventBridgeEvent<
     'Object Created',
     S3ObjectCreatedNotificationEventDetail
   >,
 ): Promise<APIGatewayProxyResult> => {
-  console.dir(event, { depth: Infinity });
+  // const s3Client = new S3Client({});
+  const transcribeClient = new TranscribeClient({ region: event.region });
 
-  // const record: SQSRecord = event.Records[0];
-  // const message = JSON.parse(record.body);
+  const { bucket, object } = event.detail;
 
-  // console.log({ message });
+  const jobName = `Transcribe-${object.key}`;
 
-  // const s3Bucket = message.Records[0].s3.bucket.name;
-  // const s3Key = message.Records[0].s3.object.key;
-  // const jobName = `Transcribe-${s3Key}`;
+  const params: StartTranscriptionJobCommandInput = {
+    TranscriptionJobName: jobName,
+    LanguageCode: 'en-US',
+    MediaFormat: 'mp4',
+    Media: { MediaFileUri: `s3://${bucket.name}/${object.key}` },
+    OutputBucketName: bucket.name,
+  };
 
-  // const params: StartTranscriptionJobCommandInput = {
-  //   TranscriptionJobName: jobName,
-  //   LanguageCode: 'en-US',
-  //   MediaFormat: 'mp4',
-  //   Media: { MediaFileUri: `s3://${s3Bucket}/${s3Key}` },
-  //   OutputBucketName: s3Bucket,
-  // };
+  console.log({ params });
 
-  // console.log({ params });
+  try {
+    const command = new StartTranscriptionJobCommand(params);
 
-  return { statusCode: 400, body: JSON.stringify({ error: 'Test request' }) };
+    const data = await transcribeClient.send(command);
 
-  // try {
-  //   const command = new StartTranscriptionJobCommand(params);
-  //   const result = await transcribeClient.send(command);
-  //   console.log({ result });
-  //   return { statusCode: 200, body: JSON.stringify(result) };
-  // } catch (error) {
-  //   console.error('Error starting transcription job:', error);
-  //   return {
-  //     statusCode: 500,
-  //     body: JSON.stringify({ error: (error as Error).message }),
-  //   };
-  // }
+    return { statusCode: 200, body: JSON.stringify(data) };
+  } catch (error) {
+    console.error('Error starting transcription job:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: (error as Error).message }),
+    };
+  }
 };
