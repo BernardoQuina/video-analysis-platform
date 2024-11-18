9dc43b2e25bbfe14ed5e7c4f94132318e7d705d4
Bernardo Quina <bquina@primeit.pt>
2024-11-18 17:57:37 +0000

    frontend - finish video card

--- apps/frontend/src/components/video-card.tsx
+++ apps/frontend/src/components/video-card.tsx
@@ -1,7 +1,13 @@
+import { useMemo } from 'react';
 import Image from 'next/image';
 import { inferProcedureOutput } from '@trpc/server';
+import moment from 'moment';
+import { Cpu, Eye } from 'lucide-react';
 
 import { AppRouter } from '../../../api/src/routers/index.router';
+import { cn } from '../utils/cn';
+
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 
 type Video = inferProcedureOutput<AppRouter['videos']['myVideos']>[number];
 
@@ -10,17 +16,66 @@ type VideoCardProps = {
 };
 
 export function VideoCard({ video }: VideoCardProps) {
+  const formattedTime = useMemo(
+    () => moment(video.createdAt).fromNow(),
+    [video],
+  );
+  const completedJobs = useMemo(() => {
+    let count = 0;
+
+    if (video.transcriptResult) count++;
+    if (video.rekognitionObjects) count++;
+    if (video.analysisResult) count++;
+
+    return count;
+  }, [video]);
+
   return (
-    <div className="relative aspect-video w-full justify-end rounded-md bg-[url()] sm:w-[calc(50%-0.5rem)] lg:w-[calc(33.33%-0.69rem)]">
+    <div className="group relative aspect-video w-full justify-end overflow-hidden rounded-lg border bg-[url()] shadow-sm sm:w-[calc(50%-0.5rem)] lg:w-[calc(33.33%-0.69rem)]">
       <Image
-        className="absolute left-0 h-full w-full"
+        className="absolute left-0 h-full w-full object-cover transition duration-300 ease-in-out group-hover:scale-105"
         alt={`${video.fileName} thumbnail`}
         src={`${process.env.NEXT_PUBLIC_MEDIA_URL}/${video.thumbnailS3Key}`}
         width={900}
         height={600}
       />
-      <div className="bg-background/95 supports-[backdrop-filter]:bg-background/60 backdrop-blur">
-        <h3>{video.fileName}</h3>
+      <div className="supports-[backdrop-filter]:bg-background-dark/30 absolute left-4 top-3 z-10 flex-row items-center gap-1.5 rounded-full p-1 pr-2 backdrop-blur-md">
+        <Avatar className="h-6 w-6 cursor-pointer">
+          <AvatarImage src={video.userPicture} alt="user picture" />
+          <AvatarFallback>
+            {video.userName.split(' ')[0]}
+            {video.userName.split(' ')[1]}
+          </AvatarFallback>
+        </Avatar>
+        <span className="text-primary-dark text-xs font-medium">
+          {video.userName}
+        </span>
+      </div>
+      <div className="supports-[backdrop-filter]:bg-background-dark/30 absolute right-4 top-3 z-10 flex-row items-center gap-1.5 rounded-full p-2 backdrop-blur-md">
+        <Cpu
+          className={cn('text-primary-dark h-4 w-4', {
+            'animate-spring-spin': completedJobs !== 3,
+          })}
+        />
+        <span className="text-primary-dark text-xs font-medium">
+          {completedJobs}/3 jobs
+        </span>
+      </div>
+      <div className="supports-[backdrop-filter]:bg-background-dark/30 px-4 py-2 backdrop-blur-md">
+        <h3 className="text-primary-dark text-sm font-medium">
+          {video.fileName}
+        </h3>
+        <div className="flex-row items-center gap-4">
+          <span className="text-muted-foreground-dark text-xs font-medium">
+            {formattedTime}
+          </span>
+          <div className="flex-row items-center gap-0.5">
+            <Eye className="text-muted-foreground-dark h-3 w-3" size="sm" />
+            <span className="text-muted-foreground-dark text-xs font-medium">
+              Public
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -14,50 +14,83 @@ const config: Config = {
       },
       colors: {
         background: 'hsl(var(--background))',
+        'background-dark': 'hsl(var(--dark-background))',
         foreground: 'hsl(var(--foreground))',
+        'foreground-dark': 'hsl(var(--dark-foreground))',
         card: {
           DEFAULT: 'hsl(var(--card))',
+          dark: 'hsl(var(--dark-card))',
           foreground: 'hsl(var(--card-foreground))',
+          'foreground-dark': 'hsl(var(--dark-card-foreground))',
         },
         popover: {
           DEFAULT: 'hsl(var(--popover))',
+          dark: 'hsl(var(--dark-popover))',
           foreground: 'hsl(var(--popover-foreground))',
+          'foreground-dark': 'hsl(var(--dark-popover-foreground))',
         },
         primary: {
           DEFAULT: 'hsl(var(--primary))',
+          dark: 'hsl(var(--dark-primary))',
           foreground: 'hsl(var(--primary-foreground))',
+          'foreground-dark': 'hsl(var(--dark-primary-foreground))',
         },
         secondary: {
           DEFAULT: 'hsl(var(--secondary))',
+          dark: 'hsl(var(--dark-secondary))',
           foreground: 'hsl(var(--secondary-foreground))',
+          'foreground-dark': 'hsl(var(--dark-secondary-foreground))',
         },
         muted: {
           DEFAULT: 'hsl(var(--muted))',
+          dark: 'hsl(var(--dark-muted))',
           foreground: 'hsl(var(--muted-foreground))',
+          'foreground-dark': 'hsl(var(--dark-muted-foreground))',
         },
         accent: {
           DEFAULT: 'hsl(var(--accent))',
+          dark: 'hsl(var(--dark-accent))',
           foreground: 'hsl(var(--accent-foreground))',
+          'foreground-dark': 'hsl(var(--dark-accent-foreground))',
         },
         destructive: {
           DEFAULT: 'hsl(var(--destructive))',
+          dark: 'hsl(var(--dark-destructive))',
           foreground: 'hsl(var(--destructive-foreground))',
+          'foreground-dark': 'hsl(var(--dark-destructive-foreground))',
         },
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
+        border: {
+          DEFAULT: 'hsl(var(--border))',
+          dark: 'hsl(var(--dark-border))',
+        },
+        input: {
+          DEFAULT: 'hsl(var(--input))',
+          dark: 'hsl(var(--dark-input))',
+        },
+        ring: {
+          DEFAULT: 'hsl(var(--ring))',
+          dark: 'hsl(var(--dark-ring))',
+        },
         chart: {
           '1': 'hsl(var(--chart-1))',
+          '1-dark': 'hsl(var(--dark-chart-1))',
           '2': 'hsl(var(--chart-2))',
+          '2-dark': 'hsl(var(--dark-chart-2))',
           '3': 'hsl(var(--chart-3))',
+          '3-dark': 'hsl(var(--dark-chart-3))',
           '4': 'hsl(var(--chart-4))',
+          '4-dark': 'hsl(var(--dark-chart-4))',
           '5': 'hsl(var(--chart-5))',
+          '5-dark': 'hsl(var(--dark-chart-5))',
         },
       },
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
+      },
+      backdropBlur: {
+        xs: '2px',
       },
       keyframes: {
         rainbow: {
@@ -69,11 +102,38 @@ const config: Config = {
           '50%': { 'background-position': '100%', filter: 'blur(0px)' },
           '100%': { 'background-position': '200%', filter: 'blur(4px)' },
         },
+        'spring-spin': {
+          '0%, 100%': {
+            transform: 'rotate(0deg)', // 1st Rotation start (and 2nd rotation end)
+          },
+          '15%': {
+            transform: 'rotate(405deg) scale(1.1)', // 1st rotation peak (over rotation)
+          },
+          '20%': {
+            transform: 'rotate(360deg)', // 1st rotation end (start hold)
+          },
+          '45%': {
+            transform: 'scale(1)', // Continue holding rotation (pulsate start)
+          },
+          '50%': {
+            transform: 'scale(1.2)', // Continue holding rotation (pulsate peak)
+          },
+          '55%': {
+            transform: 'scale(1)', // Continue holding rotation (pulsate end)
+          },
+          '80%': {
+            transform: 'rotate(360deg)', // 2nd rotation start
+          },
+          '95%': {
+            transform: 'rotate(-90deg) scale(1.1)', // 2nd rotation peak (over rotation)
+          },
+        },
       },
-      animation: {
-        rainbow: 'rainbow 2s infinite ease-in-out',
-        'rainbow-pulsate': 'rainbow-pulsate 2s infinite ease-in-out',
-      },
+    },
+    animation: {
+      rainbow: 'rainbow 2s infinite ease-in-out',
+      'rainbow-pulsate': 'rainbow-pulsate 2s infinite ease-in-out',
+      'spring-spin': 'spring-spin 5s ease-in-out infinite',
     },
   },
   plugins: [require('tailwindcss-animate')],
