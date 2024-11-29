67aea45f093e9cdba3f0c74279b4cebbb2c2ceaf
Bernardo Quina <bquina@primeit.pt>
2024-11-29 12:50:51 +0000

    Transcribe lambda - minor prompt tweak

--- apps/transcribe-lambda/src/index.ts
+++ apps/transcribe-lambda/src/index.ts
@@ -151,7 +151,7 @@ export const handler = async (
     // Prompt message
     const sqsPromptMessage = {
       video_s3_uri,
-      prompt: `${hasTranscript ? `\n${transcript}Answer the following prompt in a detailed and extensive manner (use at least 3 paragraphs), double breaking line between paragraphs and taking the transcript into account:\n` : 'Answer the following prompt in a detailed and extensive manner, double breaking line between paragraphs:\n'}${videoItem.prompt}`,
+      prompt: `${hasTranscript ? `\n${transcript}Answer the following prompt in a detailed and extensive manner (use at least 2 paragraphs), double breaking line between paragraphs and taking the transcript into account:\n` : 'Answer the following prompt in a detailed and extensive manner (use at least 2 paragraphs), double breaking line between paragraphs:\n'}${videoItem.prompt}`,
       field_name: 'promptResult', // for dynamodb field storage
     };
 
